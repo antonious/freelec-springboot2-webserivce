9ff84a77a22402b2b1360d62874c6a4e728c850f
이시종 <ecb7706@gmail.com>
2020-12-15 17:50:16 +0900

    second commit

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -28,10 +28,23 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
-
-
-
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "test";
+        int amount  = 1000;
+
+        mvc.perform(
+                get("/hello/dto").param("name",name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount",is(amount))
+
+        );
+    }
+
+
 }
 
 
